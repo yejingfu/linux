bd903ea57b6b7ada1160bf25d28ac0975a465dea
yejingfu <yejingfu@hotmail.com>
2014-03-18 09:38:46 +0000

    boost samples

---
+++ boost/src/helloworld.cpp
@@ -0,0 +1,14 @@
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+}
